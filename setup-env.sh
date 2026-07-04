b836b29536f00f9f75060291d9ed87a3df6d447c
Giovanni Santini <santigio2003@gmail.com>
2026-07-04 18:13:33 +0200

    setup: reworked setup-env script
    Signed-off-by: Giovanni Santini <santigio2003@gmail.com>

--- setup-env.sh
+++ setup-env.sh
@@ -1,32 +1,106 @@
-#!/bin.sh
+#!/bin/bash
 
-# First time setup
+TOP_DIR="$(pwd)"
+BITBAKE_SETUP_DIR="poky-wrynose"
+BUILD_DIR="${TOP_DIR}/bitbake-builds/${BITBAKE_SETUP_DIR}/"
 
-set -e
+print_help () {
+    echo "usage:"
+    echo ". ./setup-env.sh <flags>"
+    echo ""
+    echo "Flags:"
+    echo "    -f,--force   Force sync, runs bitbake-setup init again. Do this"
+    echo "                 if you changed something in configs/"
+    echo "    -h,--help    Print this help message"
+}
+
+info () {
+    echo "INFO: $1"
+}
+
+# Flags
+FORCE_SYNC=0
+
+for arg in "$@"; do
+    if [ "$arg" = "-h" ] || [ "$arg" = "--help" ]; then
+        print_help
+        exit 0
+    fi
+    if [ "$arg" = "-f" ] || [ "$arg" = "--force" ]; then
+        FORCE_SYNC=1
+        info "Using force sync"
+    fi
+done
+
 
 if [ "$3" = "$0" ] || [ "$(basename "$0")" = "setup-env.sh" ]; then
     echo "ERROR: You must SOURCE this script, do not execute it directly!"
-    echo "Correct usage: . ./setup-env.sh"
+    print_help
     exit 1
 fi
 
-echo "--> Initializing Yocto Environment via bitbake-setup..."
+BITBAKE_INIT_ARGS="--non-interactive \
+        ${TOP_DIR}/configs/poky-wrynose.conf.json \
+        poky \
+        machine/qemux86-64 \
+        distro/poky"
 
-./bitbake/bin/bitbake-setup init \
-    --non-interactive \
-    --setup-dir-name poky-wrynose \
-    ./bitbake/default-registry/configurations/poky-wrynose.conf.json \
-    poky \
-    machine/qemux86-64 \
-    distro/poky
+if [ ! -d "${BUILD_DIR}" ]; then
 
-cd bitbake-builds/poky-wrynose/ || return 1
+    echo "--> Initializing Yocto Environment via bitbake-setup..."
 
-. ./build/init-build-env
+    "${TOP_DIR}/bitbake/bin/bitbake-setup" init \
+        --setup-dir-name ${BITBAKE_SETUP_DIR} \
+        ${BITBAKE_INIT_ARGS} \
+
+    if [ $? -ne 0 ]; then
+        echo "ERROR: bitbake-setup init failed"
+        return 1
+    fi
+
+elif [ ${FORCE_SYNC} -eq 1 ]; then
+
+    echo "--> Updating Yocto Environment via bitbake-setup..."
+
+    "${TOP_DIR}/bitbake/bin/bitbake-setup" update \
+        --setup-dir "${BUILD_DIR}" \
+        --update-bb-conf yes
+
+    if [ $? -ne 0 ]; then
+        echo "ERROR: bitbake-setup update failed"
+        return 1
+    fi
+else
+    echo "--> Existing build directory found. Skipping initialization."
+fi
+
+# Layers
+DUMMY_LAYER=meta-dummy
+
+if [ ! -d ${DUMMY_LAYER} ]; then
+    echo "--> Creating ${DUMMY_LAYER}..."
+    ${TOP_DIR}/bitbake/bin/bitbake-layers create-layer ${DUMMY_LAYER}
+fi
+
+cd "${BUILD_DIR}" || return 1
+
+if [ -f "./build/init-build-env" ]; then
+    . ./build/init-build-env
+else
+    . ./build/build-env
+fi
+
+
+if [ -d ../../../${DUMMY_LAYER} ]; then
+    echo "--> Adding ${DUMMY_LAYER} layer..."
+    bitbake-layers add-layer ../../../${DUMMY_LAYER}
+fi
 
 echo "--> Applying active configuration fragments..."
 
 bitbake-config-build enable-fragment core/yocto/root-login-with-empty-password
 bitbake-config-build enable-fragment core/yocto/sstate-mirror-cdn
 
-echo "Yocto configuration done"
+echo "-----------------------------------------------------"
+echo " Yocto environment ready. You can now run 'bitbake'."
+echo "-----------------------------------------------------"
